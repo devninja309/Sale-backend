3ce694b5738d5bf5b324c0b849fe606b75bd45ac
devninja309 <ryooyama.dev@gmail.com>
2022-04-23 15:03:01 -0700

    update credentials

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("sales_db", "root", "", {
+const db = new Sequelize("sales_db", "manual", "Butterfly123!@#", {
   host: "localhost",
   dialect: "mysql",
 });
